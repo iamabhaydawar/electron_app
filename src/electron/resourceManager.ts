f06b3c15676940880a352b140be4f5bccc8622c1
iamabhaydawar <iamabhaydawar@gmail.com>
2025-09-01 16:25:40 -0700

    2.2 Add resource management functionality with CPU, RAM, and storage polling in Electron app; update package dependencies and scripts for improved development workflow

---
+++ src/electron/resourceManager.ts
@@ -0,0 +1,48 @@
+import osUtils from 'os-utils';
+import os from 'os';
+import fs from 'fs';
+const POLLING_INTERVAL = 500;
+
+export function pollResources() {
+  setInterval(async() => {
+    const cpuUsage = await getCPUUsage();
+    const ramUsage = await getRAMUsage();
+    const storageUsage = await getStorageData();
+    console.log({cpuUsage, ramUsage, storageUsage:storageUsage.usage});
+  },POLLING_INTERVAL);
+}
+
+export function getStaticData(){
+  const totalStorage = getStorageData().total;
+  const cpuModel =os.cpus()[0].model;
+  const totalMemoryGB=Math.floor(os.totalmem()/(1024*1024*1024));
+  return{
+    totalStorage,
+    cpuModel,
+    totalMemoryGB,
+  }
+}
+
+
+function getCPUUsage() {
+  return new Promise((resolve) => {
+    osUtils.cpuUsage((percentage) => resolve(percentage));
+  });
+}
+
+function getRAMUsage() {
+  return 1-osUtils.freememPercentage();
+}
+
+
+function getStorageData() {
+  // requires node 18
+  const stats = fs.statfsSync(process.platform === 'win32' ? 'C://' : '/');
+  const total = stats.bsize * stats.blocks;
+  const free = stats.bsize * stats.bfree;
+
+  return {
+    total: Math.floor(total / 1_000_000_000),
+    usage: 1 - free / total,
+  };
+}
