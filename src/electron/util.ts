bb3bc2168cc8714e1d39792348a74909deb87032
iamabhaydawar <iamabhaydawar@gmail.com>
2025-09-04 12:17:38 -0700

    2.5 Event Validation -Enhanced Path Resolution and IPC Validation -Added a new function to resolve the UI path in the Electron app and implement frame validation in IPC communication to prevent malicious events. This improves security and maintains the integrity of the application.

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,5 +1,7 @@
-import { ipcMain, WebContents } from "electron";
+import { ipcMain, WebContents, WebFrameMain } from "electron";
 import { EventPayloadMapping } from "../../types.js";
+import { getUIPath } from "./pathResolver.js";
+import { pathToFileURL } from "url";
 
 export function isDev() {
     return process.env.NODE_ENV === 'development';
@@ -9,7 +11,10 @@ export function ipcMainHandle<Key extends keyof EventPayloadMapping>(
   key: Key,
   handler: () => EventPayloadMapping[Key]
 ) {
-  ipcMain.handle(key, () => handler());
+  ipcMain.handle(key, (event) => {
+    validateEventFrame(event.senderFrame);
+     return handler()
+  });
 }
 
 export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
@@ -20,3 +25,16 @@ export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
   webContents.send(key, payload);
 }
 
+export function validateEventFrame(frame: WebFrameMain | null) {
+  console.log(frame?.url);
+  // Handle null frame
+  if (!frame) {
+    throw new Error('Invalid frame');
+  }
+  if(isDev() && new URL(frame.url).origin == 'http://localhost:5173'){  
+    return true;
+  }
+  if(frame.url !== pathToFileURL(getUIPath()).toString()){
+    throw new Error('Malicious event');
+  }
+}
